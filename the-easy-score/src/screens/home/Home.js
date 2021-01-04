72e8bc3d9c0cd30cba91d7d9edf926c2dfd3119e
@Shevphil <Victoriasphil1@gmail.com>
2021-01-04 14:33:08 -0500

    Played around with buttons on the home page and add styling to psuedo menu

--- the-easy-score/src/screens/home/Home.js
+++ the-easy-score/src/screens/home/Home.js
@@ -8,8 +8,32 @@ const Home = (props) => {
     <main className="home-main">
       <div>
         <h2>Choose the <b id='home-emphasis'>easy</b> option</h2>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
       </div>
       <SearchForm push={props.history.push} />
+      {/* <div className="tidbitContainer">
+        <div className="bitContainer">
+         <div className="tidbitTitleContainer"> 
+          <div className="tidbitIcon"></div>
+          <p>Some Text</p> 
+         </div>
+         <p> Brief description of why use the easy score site</p>
+        </div>
+        <div className="bitContainer">
+         <div className="tidbitTitleContainer"> 
+          <div className="tidbitIcon"></div>
+          <p>Some Text</p> 
+         </div>
+         <p> Brief description of why use the easy score site</p>
+        </div>
+        <div className="bitContainer">
+         <div className="tidbitTitleContainer"> 
+          <div className="tidbitIcon"></div>
+          <p>Some Text</p> 
+         </div>
+         <p> Brief description of why use the easy score site</p>
+        </div>
+       </div>  */}
     </main>
   );
 };
