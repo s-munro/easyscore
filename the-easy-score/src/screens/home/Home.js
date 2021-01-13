fb4a3d40f930258b8368868da63fa5bc96543dc4
@Shevphil <Victoriasphil1@gmail.com>
2021-01-12 23:03:53 -0500

    Newest home screen design concept

--- the-easy-score/src/screens/home/Home.js
+++ the-easy-score/src/screens/home/Home.js
@@ -1,50 +1,39 @@
 import React from "react";
 import Nav from "../../navbar/Nav";
 import "./home.css";
-
+import SecondaryNav from "../../navbar/SecondaryNav";
 import SearchForm from "./components/SearchForm";
 
 const Home = (props) => {
   return (
     <div>
-      <Nav />
+      <SecondaryNav />
       <main className="home-main">
-        <div>
+        <div className="homePageLeftMain">
           <h2>
-            Choose the <b id="home-emphasis">easy</b> option
+            Welcome to The <b className="miniLogoEasy">Easy</b><b className="miniLogoScore">Score</b>
           </h2>
           <p>
             {" "}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.{" "}
+            We are The EasyScore -using our scoring system based on grade 
+            distribution data, we do the work of grade 
+            hunting for you and find an easy A for your courses! {" "}
           </p>
+          <h3>Choose the <b id="miniemphasiseasy">easy</b> option!</h3>
+          <div className="homeGraphicContainer">
+            <div className="homeGraphicEasyScore"></div>
+            <div className="homeGraphicArrow"></div>
+          </div>
+          <p>An <b className="miniLogoEasy">Easy</b><b className="miniLogoScore">Score</b>! <br></br>And your future grade!</p>
         </div>
-        <SearchForm push={props.history.push} />
-        {/* <div className="tidbitContainer">
-        <div className="bitContainer">
-         <div className="tidbitTitleContainer"> 
-          <div className="tidbitIcon"></div>
-          <p>Some Text</p> 
-         </div>
-         <p> Brief description of why use the easy score site</p>
+
+        <div className="homePageRightMain">
+          <div className="homePageRightBckGrd">
+            <div className="homePageImage"><img src="../home/components/computerscreen.jpg"></img></div>
+          </div>
+          <div className="homePageSlideShow"></div>
         </div>
-        <div className="bitContainer">
-         <div className="tidbitTitleContainer"> 
-          <div className="tidbitIcon"></div>
-          <p>Some Text</p> 
-         </div>
-         <p> Brief description of why use the easy score site</p>
-        </div>
-        <div className="bitContainer">
-         <div className="tidbitTitleContainer"> 
-          <div className="tidbitIcon"></div>
-          <p>Some Text</p> 
-         </div>
-         <p> Brief description of why use the easy score site</p>
-        </div>
-       </div>  */}
+        {/* <SearchForm push={props.history.push} /> */}
       </main>
     </div>
   );
