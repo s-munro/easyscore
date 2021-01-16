a9fc4cd720bd20def4f7c52fbc52215584e1c7dc
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-16 18:07:04 -0500

    revamped course card

--- the-easy-score/src/screens/searchresults/components/CourseCard.js
+++ the-easy-score/src/screens/searchresults/components/CourseCard.js
@@ -7,45 +7,31 @@ import CourseCardInstructor from "./CourseCardInstructor";
 const CourseCard = ({ course }) => {
   return (
     <div className="course-card">
-      <div className="course-card-top-half">
-        <div className="top-half-top-twothirds">
-          <div className="top-half-top-third-left-side">
-            <div className="course-rating">{course.rating}</div>
+      <div className="course-card-top-portion">
+        <div className="couse-card-top-portion-left-side">
+          <div className="course-card-easyscore">{course.rating}</div>
+        </div>
+        <div className="couse-card-top-portion-right-side">
+          <div className="course-card-credits-container">
+            <div className="course-card-credits-title">Credits</div>
+            <div className="course-card-credits">{course.credits}</div>
           </div>
-          <div className="top-half-top-third-right-side">
-            <div className="explanation">
-              Professors scored: {course.instructors.length}
+          <div className="course-card-professors-scored-container">
+            <div className="course-card-professors-scored-title">
+              Professors Scored
+            </div>
+            <div className="course-card-professors-scored">
+              {course.instructors.length}
             </div>
           </div>
         </div>
-        <Link
-          to={`/courses/${course.full_code}_${course.name.replace(/ /g, "_")}`}
-        >
-          <div className="top-half-bottom-third">
-            {course.full_code} {course.name}
-          </div>
-        </Link>
       </div>
-      <div className="course-card-bottom-half">
-        <div className="comparisonNum">Credit Hours ({course.credits})</div>
-        <div className="course-card-professors-container">
-          {course.instructors.length > 3
-            ? course.instructors.slice(0, 3).map((professor) => {
-                return (
-                  <CourseCardInstructor
-                    professor={professor}
-                    key={professor.name}
-                  />
-                );
-              })
-            : course.instructors.map((professor) => {
-                return (
-                  <CourseCardInstructor
-                    professor={professor}
-                    key={professor.name}
-                  />
-                );
-              })}
+      <div className="course-card-bottom-sections">
+        <div className="course-card-course-title">
+          {course.full_code}: {course.name}
+        </div>
+        <div className="course-card-click-more">
+          <Link>More -></Link>
         </div>
       </div>
     </div>
