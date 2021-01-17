e84d15f597f44701fdc5245b99a3bbf5e1b09e32
@Shevphil <Victoriasphil1@gmail.com>
2021-01-16 23:55:30 -0500

    not sure what changes will be overwritten

--- the-easy-score/src/screens/home/Home.js
+++ the-easy-score/src/screens/home/Home.js
@@ -17,7 +17,7 @@ const Home = (props) => {
       <Nav />
       <main>
         <div className="logoTitleContainer">
-         <h1><b>Easy</b><span className="scoreLogo">Score</span></h1>
+         <h1 className="logoTitle"><b>Easy</b><span className="scoreLogo">Score</span></h1>
         </div>
         <SearchForm />
       </main>
