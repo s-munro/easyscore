5326c36eabf94f3d115843252c4b7f4535e6f902
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-17 14:38:22 -0500

    transformProfessorName function created & implemented

--- the-easy-score/src/screens/coursepage/components/ProfessorCard.js
+++ the-easy-score/src/screens/coursepage/components/ProfessorCard.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Card } from "react-bootstrap";
 
 import "../course.css";
 
 const ProfessorCard = ({ instructor }) => {
+  const [transformedName, setTransformedName] = useState(instructor.name);
+
+  useEffect(() => {
+    transformProfessorName(instructor.name);
+  }, []);
+
+  const transformProfessorName = (instructorName) => {
+    const nameArray = instructorName.split(",");
+    const firstName = nameArray[0];
+    const lastNameString = nameArray[1];
+    const lastNameInitial = lastNameString.charAt(0);
+    setTransformedName(`${firstName}, ${lastNameInitial}.`);
+  };
+
   return (
     <div className="w-100">
       <Card>
         <div>
           {instructor.rating}
-          {instructor.name}
+          {transformedName}
           {instructor.average_grades[0]}%{instructor.average_grades[1]}%
           {instructor.average_grades[2]}%{instructor.average_grades[3]}%
           {instructor.semesters_taught}
