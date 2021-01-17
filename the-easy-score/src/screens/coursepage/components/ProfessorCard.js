937af0496c1c5774201833a0d2abeda632535b30
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-17 04:33:28 -0500

    search page nav now hits axiosurl and re-renders courses on submit

--- the-easy-score/src/screens/coursepage/components/ProfessorCard.js
+++ the-easy-score/src/screens/coursepage/components/ProfessorCard.js
@@ -1,17 +1,22 @@
 import React from "react";
+
+import { Card } from "react-bootstrap";
+
 import "../course.css";
 
 const ProfessorCard = ({ instructor }) => {
   return (
     <div className="w-100">
-      <div>
-        {instructor.rating}
-        {instructor.name}
-        {instructor.average_grades[0]}%{instructor.average_grades[1]}%
-        {instructor.average_grades[2]}%{instructor.average_grades[3]}%
-        {instructor.semesters_taught}
-        {instructor.average_number_of_students}
-      </div>
+      <Card>
+        <div>
+          {instructor.rating}
+          {instructor.name}
+          {instructor.average_grades[0]}%{instructor.average_grades[1]}%
+          {instructor.average_grades[2]}%{instructor.average_grades[3]}%
+          {instructor.semesters_taught}
+          {instructor.average_number_of_students}
+        </div>
+      </Card>
     </div>
   );
 };
