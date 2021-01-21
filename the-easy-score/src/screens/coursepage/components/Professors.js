0d4e4d27308fe94edd187056f6eb9dfc781b61a8
@Shevphil <Victoriasphil1@gmail.com>
2021-01-21 18:41:24 -0500

    Fixed sizing issues with prof cards when there are multiples or only one. Added content space for years taught and days teaching

--- the-easy-score/src/screens/coursepage/components/Professors.js
+++ the-easy-score/src/screens/coursepage/components/Professors.js
@@ -4,14 +4,14 @@ import ProfessorCard from "./ProfessorCard";
 const Professors = (props) => {
   // console.log("professors prof", props.currentInstructors);
   return (
-    <div className="row ">
+    <div className="testTest">
       {props.currentInstructors.map((instructor, index) => {
         return (
           <div
             key={index}
             className="col-xl-10 col-lg-10 col-md-10 col-sm-12 col-12 mb-5"
           >
-            <ProfessorCard className="profTest" instructor={instructor} key={index} />
+            <ProfessorCard  instructor={instructor} key={index} />
           </div>
         );
       })}
