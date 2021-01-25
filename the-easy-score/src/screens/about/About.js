2708c0d64a7149b8a728617c072d85753d2785e8
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-25 00:24:58 -0500

    about page scaffolding

--- the-easy-score/src/screens/about/About.js
+++ the-easy-score/src/screens/about/About.js
@@ -1,13 +1,25 @@
-import React from "react";
-import Sidebar from "../../components/Sidebar.js";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { setNavStyle, setFooterStyle } from "../../actions/index";
+// import Sidebar from "../../components/Sidebar.js";
 
-const About = () => {
+const About = (props) => {
+  useEffect(() => {
+    props.setNavStyle(3);
+    props.setFooterStyle(1);
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <div>About</div>
-      <Sidebar />
+      {/* <Sidebar /> */}
     </div>
   );
 };
-
-export default About;
+const mapStateToProps = (state) => {
+  return {
+    navStyle: state.navStyle,
+    footerStyle: state.footerStyle,
+  };
+};
+export default connect(mapStateToProps, { setNavStyle, setFooterStyle })(About);
