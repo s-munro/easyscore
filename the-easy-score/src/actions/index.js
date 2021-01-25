c403c1649321ef5beec3e049428a3b03e8a92b9b
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-24 20:26:24 -0500

    state for footer set up

--- the-easy-score/src/actions/index.js
+++ the-easy-score/src/actions/index.js
@@ -1,5 +1,10 @@
 export const SET_NAV_STYLE = "SET_NAV_STYLE";
+export const SET_FOOTER_STYLE = "SET_FOOTER_STYLE";
 
 export const setNavStyle = (style) => {
-  return { type: SET_NAV_STYLE, payload: style };
+  return { type: SET_NAV_STYLE, payload: parseInt(style) };
+};
+
+export const setFooterStyle = (style) => {
+  return { type: SET_FOOTER_STYLE, payload: parseInt(style) };
 };
