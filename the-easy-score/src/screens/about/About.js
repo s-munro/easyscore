b93d02aca602a051685b1082c9038d95a58302d1
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-24 22:37:08 -0500

    sidebar

--- the-easy-score/src/screens/about/About.js
+++ the-easy-score/src/screens/about/About.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Sidebar from "../../components/Sidebar.js";
 
 const About = () => {
   return (
     <div>
       <div>About</div>
+      <Sidebar />
     </div>
   );
 };
