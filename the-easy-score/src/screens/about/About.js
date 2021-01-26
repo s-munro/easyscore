ad070f7299795f1021108d5dcfd442d312df1ef8
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-26 14:48:04 -0500

    about page

--- the-easy-score/src/screens/about/About.js
+++ the-easy-score/src/screens/about/About.js
@@ -2,17 +2,66 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { setNavStyle, setFooterStyle } from "../../actions/index";
 // import Sidebar from "../../components/Sidebar.js";
+import "./About.css";
 
 const About = (props) => {
   useEffect(() => {
     props.setNavStyle(3);
-    props.setFooterStyle(1);
+    props.setFooterStyle(3);
   }, []);
 
   return (
     <div className="container">
-      <div>About</div>
-      {/* <Sidebar /> */}
+      <div className="row-100 mt-5">
+        <div className="col-12 d-flex justify-content-center align-items-center">
+          <h2 className="about-title">About</h2>
+        </div>
+        <hr />
+        <div className="row w-100">
+          <div className="col">
+            <div className="about-text">
+              <p>
+                Here at EasyScore, we believe in working smarter, not harder. We
+                accomplish this by using our intuitive and intelligent
+                algorithm. We just happen to help you find the easiest teacher
+                for each of your courses.
+              </p>
+              <p>Happy Course Hunting!</p>
+              <p>
+                Using previous grade distributions we accurately predict how
+                easily a specific teacher grades for each taught course on a
+                scale from 0 to 100. The number next to each instructor is the
+                instructor's score for that course and the number next to each
+                course is the average score of all the teachers who have taught
+                that course.
+              </p>
+              <p>
+                <span className="disclaimer">
+                  Disclaimer: This site is not designed to be used solely for
+                  selecting courses. View other resources for your courses
+                  selection. This site is not officially affiliated with Indiana
+                  University.
+                </span>
+              </p>
+              <p className="small">
+                **Results beyond 40 have been truncated **Teachers with fewer
+                semesters taught and fewer students have been scored down
+              </p>
+              <p>
+                <span className="source-text small">
+                  Source: Official IU Grade Distribution website
+                  <a
+                    href="gradedistribution.registrar.indiana.edu/"
+                    className="grade-dist-link"
+                  >
+                    gradedistribution.registrar.indiana.edu/
+                  </a>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
