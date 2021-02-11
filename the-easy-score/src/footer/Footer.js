a20cc0d7578990eea455ec673322d39549795f2b
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-10 23:34:23 -0500

    chore: delete commented-out css

--- the-easy-score/src/footer/Footer.js
+++ the-easy-score/src/footer/Footer.js
@@ -8,37 +8,33 @@ const Footer = (props) => {
   //
   if (props.footerStyle === 1) {
     return (
-      <footer className="container">
-        <div className="row w-100">
-          <div className="footer-container">
-            <p>
-              <small>
-                © Copyright 2021 The EasyScore. All rights reserved.
-              </small>
-            </p>
-          </div>
+      <footer className="row w-100 d-flex flex-column justify-content-end align-items-center">
+        <div className="col-12 d-flex justify-content-center align-items-center footer-container">
+          <p>
+            <small>© Copyright 2021 The EasyScore. All rights reserved.</small>
+          </p>
         </div>
       </footer>
     );
   } else if (props.footerStyle === 2) {
     return (
-      <footer className="secondaryFooter">
-        <div>
+      <footer className="row w-100 secondaryFooter d-flex justify-content-center">
+        <div className="col-12 d-flex justify-content-center">
           <p>
             <small>© Copyright 2021 The EasyScore. All rights reserved.</small>
           </p>
+          {/* </div> */}
+          <Link className="aboutFooterLink" to="/about">
+            <div>
+              <small>About</small>
+            </div>
+          </Link>
+          <Link className="" to="/contact">
+            <div>
+              <small>Contact</small>
+            </div>
+          </Link>
         </div>
-        <Link className="aboutFooterLink" to="/about">
-          <div>
-            <small>About</small>
-          </div>
-        </Link>
-        <Link className="" to="/contact">
-          <div>
-            <small>Contact</small>
-          </div>
-        </Link>
-        {/* <div></div> */}
       </footer>
     );
   } else if (props.footerStyle === 3) {
