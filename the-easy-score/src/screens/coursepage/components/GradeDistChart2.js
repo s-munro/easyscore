2b547efc749f4f42479b4fc195a2680d31c6ca07
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-10 22:42:20 -0500

    chore: remove unused imports

--- the-easy-score/src/screens/coursepage/components/GradeDistChart2.js
+++ the-easy-score/src/screens/coursepage/components/GradeDistChart2.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
-import ChartDataLabels from "chartjs-plugin-datalabels";
 
 const GradeDistChart2 = ({ average_grades, pixels }) => {
   let data = {
